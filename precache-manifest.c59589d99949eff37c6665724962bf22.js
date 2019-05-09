7703e7e54b27a4cbe53c97319445c47569c47cae
ailerle <akinilerle@gmail.com>
2019-05-09 16:57:42 +0300

    Updates

---
+++ precache-manifest.c59589d99949eff37c6665724962bf22.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc223dc13887010aea59",
+    "url": "/Cmpe492/static/css/main.4482d798.chunk.css"
+  },
+  {
+    "revision": "bc223dc13887010aea59",
+    "url": "/Cmpe492/static/js/main.bc223dc1.chunk.js"
+  },
+  {
+    "revision": "ee273ce6ea32604d1ac7",
+    "url": "/Cmpe492/static/css/1.12b89eeb.chunk.css"
+  },
+  {
+    "revision": "ee273ce6ea32604d1ac7",
+    "url": "/Cmpe492/static/js/1.ee273ce6.chunk.js"
+  },
+  {
+    "revision": "9581dd2843a15817a3f5",
+    "url": "/Cmpe492/static/css/2.3ffc5def.chunk.css"
+  },
+  {
+    "revision": "9581dd2843a15817a3f5",
+    "url": "/Cmpe492/static/js/2.9581dd28.chunk.js"
+  },
+  {
+    "revision": "c7e7ae506e4774351ac6",
+    "url": "/Cmpe492/static/js/3.c7e7ae50.chunk.js"
+  },
+  {
+    "revision": "06e5d5429677d58a13d5",
+    "url": "/Cmpe492/static/js/runtime~main.06e5d542.js"
+  },
+  {
+    "revision": "96ca860ac539d1dbb6e2aec654f60552",
+    "url": "/Cmpe492/index.html"
+  }
+];
